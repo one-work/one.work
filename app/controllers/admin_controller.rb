1e9d487e29d18db2e2b6bea5213a2bc632eeb292
qinmingyuan <mingyuan0715@foxmail.com>
2025-08-09 12:51:20 +0800

    admin logic

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,4 +4,9 @@ class AdminController < ApplicationController
   include Roled::Controller::Admin
   before_action :require_org_member, :require_role
 
+  def xxx
+    @orders_count = Order.default_where(default_params).count
+    @items_count = Item.default_where(default_params).count
+  end
+
 end
