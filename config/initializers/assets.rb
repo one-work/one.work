4f59b1187a3f85c15ca37af4b13a5c10c7aed6d1
qinmingyuan <mingyuan0715@foxmail.com>
2025-10-13 02:07:55 +0800

    moved unused

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,6 +1,5 @@
 Rails.configuration.assets.paths += [
-  Rails.root.join('node_modules/@fortawesome/fontawesome-free/webfonts'),
-  Rails.root.join('app/assets/images'),
+  Rails.root.join('app/assets/images')
 ]
 
 Rails.configuration.assets.excluded_paths << ActionText::Engine.root.join('app/assets/javascripts')
