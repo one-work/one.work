f7fd3b533af182a30e24cd57c4123aaed4c6e4d0
qinmingyuan <mingyuan0715@foxmail.com>
2025-10-19 16:00:09 +0800

    simple files

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,5 +2,13 @@ Rails.configuration.assets.paths += [
   Rails.root.join('app/assets/images')
 ]
 
-Rails.configuration.assets.excluded_paths << ActionText::Engine.root.join('app/assets/javascripts')
-Rails.configuration.assets.excluded_paths << ActionView::Railtie.root.join('lib/assets/compiled')
+Rails.configuration.assets.excluded_paths += [
+  Rails.root.join('app/assets/builds'),
+  ActionView::Railtie.root.join('app/assets/javascripts'),
+  ActionText::Engine.root.join('app/assets/javascripts'),
+  ActionCable::Engine.root.join('app/assets/javascripts'),
+  ActiveStorage::Engine.root.join('app/assets/javascripts'),
+  Turbo::Engine.root.join('app/assets/javascripts'),
+  Trix::Engine.root.join('app/assets/javascripts'),
+  Trix::Engine.root.join('app/assets/stylesheets')
+]
