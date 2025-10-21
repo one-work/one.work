8a4a204ed394d86feb48ad7b554bd2e80095f114
qinmingyuan <mingyuan0715@foxmail.com>
2025-10-21 21:50:36 +0800

    deploy

--- app/models/crm/maintain.rb
+++
@@ -1,7 +0,0 @@
-module Crm
-  class Maintain < ApplicationRecord
-    include Model::Maintain
-    include Wechat::Ext::Maintain
-    include Eventual::Ext::Planned if defined? RailsEvent
-  end
-end
