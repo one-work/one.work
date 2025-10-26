a19469f12b7351f43d6bf5a47492a50412ad8018
qinmingyuan <mingyuan0715@foxmail.com>
2025-10-27 00:26:41 +0800

    com

--- config/initializers/event_subscriber.rb
+++ config/initializers/event_subscriber.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
-subscriber = EventJsonSubscriber.new
 
-Rails.event.subscribe(subscriber) do |event|
-  event[:name].start_with?('controller.')
-end
 
 Rails.application.config.after_initialize do
+  subscriber = EventJsonSubscriber.new
   subscriber.task.execute
+
+  Rails.event.subscribe(subscriber) do |event|
+    event[:name].start_with?('controller.')
+  end
 end
