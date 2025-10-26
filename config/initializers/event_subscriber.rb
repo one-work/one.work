d91944bb0fcbdc84dff6143a9dd891f5f8fc8f44
qinmingyuan <mingyuan0715@foxmail.com>
2025-10-26 23:05:28 +0800

    event

--- config/initializers/event_subscriber.rb
+++ config/initializers/event_subscriber.rb
@@ -4,5 +4,6 @@ subscriber = EventJsonSubscriber.new
 Rails.event.debug_mode = true
 Rails.event.raise_on_error = true
 Rails.event.subscribe(subscriber) do |event|
+  Rails.logger.debug "------------------------------------------#{event}--"
   event[:name].start_with?('controller.')
 end
