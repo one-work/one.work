40a23ee6713118783382fb4b84874907a465ab7a
qinmingyuan <mingyuan0715@foxmail.com>
2025-10-26 22:14:22 +0800

    raise on error

--- config/initializers/event_subscriber.rb
+++ config/initializers/event_subscriber.rb
@@ -2,6 +2,7 @@
 subscriber = EventJsonSubscriber.new
 
 Rails.event.debug_mode = true
+Rails.event.raise_on_error = true
 Rails.event.subscribe(subscriber) do |event|
   event[:name].start_with?('controller.')
 end
