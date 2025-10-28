033bf688d8e050154470494e6385b1507c1c3a85
qinmingyuan <mingyuan0715@foxmail.com>
2025-10-29 01:07:54 +0800

    prod

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -19,9 +19,9 @@ before_worker_boot do
   ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
 
   # Rails.event.debug_mode = true
-  # Rails.event.subscribe(EventRequestSubscriber.new) do |event|
-  #   event[:name].start_with?('controller.')
-  # end
+  Rails.event.subscribe(EventRequestSubscriber.new) do |event|
+    event[:name].start_with?('controller.')
+  end
   #
   # Rails.event.subscribe(EventSqlSubscriber.new) do |event|
   #   ['active_record.sql'].include?(event[:name])
